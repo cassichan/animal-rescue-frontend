2a5fdfa73ba689c6250dab6678dc8d3d6f548951
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-29 20:55:58 -0400

    centered hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
+import "../Styles/Hero.css"
+
 export default function Hero () {
     return (
-        <>
-        <h1>Chance 4 Pets</h1>
-        <p>
+        <div className="hero">
+        <h1>Chance 4 Pets</h1><br/><br/>
+        <p className="hero-text">
             Mission: To make it easier for humans to help pets. A bridge between animal rescues, shelters and those needing their services. Help pets in need find their forever home, and humans find their next furbaby.
         </p>
-        </>
+        </div>
     )
 }
