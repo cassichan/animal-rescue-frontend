e53182a739b25c79fb676198b3fb6c90bc793f7d
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-09-04 15:05:40 -0400

    added logo and updated read me

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Credits
 
-Pure CSS Cat Animation: Copyright (c) 2022 by Johan Mouchet (https://codepen.io/johanmouchet/pen/OXxvqM)
+1. Logo created by Alex Pelta using rucraft
+
+2. Pure CSS Cat Animation: Copyright (c) 2022 by Johan Mouchet (https://codepen.io/johanmouchet/pen/OXxvqM)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,20 +1,54 @@
 import { Link } from "react-router-dom";
-import Socials from "./Socials"
-import "../../App.css"
+// import Socials from "./Socials";
+import "../../App.css";
 
 export default function Navbar() {
   return (
     <>
       <nav className="header">
-        <Socials />
-        <div className="link-container">
+        <section className="header-logo-container">
+          <img
+            className="brand-logo-image"
+            src="chance4petslogo.png"
+            alt="Logo of black cat and black dog animation on top of a cupped hand"
+          ></img>
+        </section>
+
+        <section className="link-container">
           <Link className="route-links" to="/">
             Home
           </Link>
           <Link className="route-links" to="/found-a-pet">
             Share found animal
           </Link>
-        </div>
+        </section>
+        <section className="main-social-container">
+          <div className="social-container">
+            <a
+              href="https://github.com/cassichan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="logo-img"
+                src="GitHub-Mark-32px.png"
+                alt="Github logo: the silhouette of a white cat outline with a tentactle-shaped tail inside of a black circle."
+              ></img>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/cassandra-l-curcio/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="logo-img"
+                src="LI-In-Bug.png"
+                alt="Linkedin logo: 'in' in white text inside of a rounded light-royal-blue square."
+              ></img>
+            </a>
+          </div>
+          {/* <Socials /> */}
+        </section>
       </nav>
     </>
   );
